c11a3e37c2afb4537db0facc6a8a644a8fc7daed
MagdaBM <buczek.magdalena@gmail.com>
2019-01-18 17:09:29 +0100

    Add a simple_form in place off form in new views

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ApplicationRecord
   belongs_to :offer
   belongs_to :user
 
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 1000 }
   validates :nick, presence: true
 
 end
